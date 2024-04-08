21d5b12d3ce3859b50d6d43454585ef803785897
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-04-08 22:20:38 +0530

    Update Homepage.jsx

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -6,34 +6,14 @@ import Marquee from "react-fast-marquee";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import insta from "./INSTA.png"
 import fb from "./FACEBOOK.png"
 import lk from "./LINKED.png"
 import mail from "./MAIL.png";
 const HomePage = () => {
-  var settings = {
-    className: "sliderr",
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
-  var settings1 = {
-    className: "sliderr-1",
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
-  return (
+  return  (
     <>
     <Header/>
     <div className="SOCIAL">
@@ -79,84 +59,44 @@ const HomePage = () => {
     <div className="slider">
     <div className="principle">
     <h2>Pictures</h2>
-    </div>
-    <Slider {...settings}>
+    </div> 
+    <Carousel interval={1800} className="caro" infiniteLoop={true} autoPlay={true} showArrows={false} showIndicators={true} showThumbs={true} stopOnHover={true} width={"70vw"}>
     <div className="slider-card">
-    <h2>1</h2>
-    <h2>RANDOM......</h2>
-    </div>
-    <div className="slider-card">
-    <h2>2</h2>
-    <h2>RANDOM......</h2>
+        <p>PRINCIPLE 1</p>
     </div>
     <div className="slider-card">
-    <h2>3</h2>
-    <h2>RANDOM......</h2>
+        <p>PRINCIPLE 2</p>
     </div>
     <div className="slider-card">
-    <h2>4</h2>
-    <h2>RANDOM......</h2>
+        <p>PRINCIPLE 3</p>
     </div>
     <div className="slider-card">
-    <h2>5</h2>
-    <h2>RANDOM......</h2>
+        <p>PRINCIPLE 4</p>
     </div>
     <div className="slider-card">
-    <h2>6</h2>
-    <h2>RANDOM......</h2>
+        <p>PRINCIPLE 5</p>
     </div>
-    <div className="slider-card">
-    <h2>7</h2>
-    <h2>RANDOM......</h2>
-    </div>
-    <div className="slider-card">
-    <h2>8</h2>
-    <h2>RANDOM......</h2>
-    </div>
-    <div className="slider-card">
-    <h2>9</h2>
-    <h2>RANDOM......</h2>
-    </div>
-    <div className="slider-card">
-    <h2>10</h2>
-    <h2>RANDOM......</h2>
-    </div>
-    </Slider>
+</Carousel>
     </div>
     <div className="sessions">
     <h2>Our Latest Sessions</h2>
-    <Slider {...settings1}>
-    <div className="session-card">
-    <div className="session-header">
-    <h2>TITLE-1</h2>
+    <Carousel interval={1800} className="caro" infiniteLoop={true} autoPlay={true} showArrows={false} showIndicators={true} showThumbs={true} stopOnHover={true} width={"90vw"} centerMode={true} autoFocus={true} centerSlidePercentage={100}>
+    <div className="slider-card">
+        <p>SESSION 1</p>
     </div>
-    <div className="session-content">CONTENT</div>
+    <div className="slider-card">
+        <p>SESSION 2</p>
     </div>
-    <div className="session-card">
-    <div className="session-header">
-    <h2>TITLE-2</h2>
+    <div className="slider-card">
+        <p>SESSION 3</p>
     </div>
-    <div className="session-content">CONTENT</div>
+    <div className="slider-card">
+        <p>SESSION 4</p>
     </div>
-    <div className="session-card">
-    <div className="session-header">
-    <h2>TITLE-3</h2>
+    <div className="slider-card">
+        <p>SESSION 5</p>
     </div>
-    <div className="session-content">CONTENT</div>
-    </div>
-    <div className="session-card">
-    <div className="session-header">
-    <h2>TITLE-4</h2>
-    </div>
-    <div className="session-content">CONTENT</div>
-    </div>
-    <div className="session-card">
-    <div className="session-header">
-    <h2>TITLE-5</h2>
-    </div>
-    <div className="session-content">CONTENT</div>
-    </div>
-    </Slider>
+</Carousel>
     </div>
     <div className="skills" id="SKILLS">
     <div className="skills-1">
