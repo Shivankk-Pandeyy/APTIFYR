54e47f317a0d6d9ce1955b081da1078f3d0266b5
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-04-20 10:40:08 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,26 +1,78 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import "./Pages.css"
 import { NavLink } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import Mouse from "./Mouse";
+import axios from "axios";
+import Header from "../Components/Header";
+import PIC from './LOGIN.png'
 const Login = () => {
-  const notify = () => toast.success('Here is your toast.');
+  const notify1 = () => toast.error('All Fields are Mandatory!');
+  const notify2 = () => toast.success('Login Successfull!');
+  const notify3 = () => toast.error('Invalid Password!');
+  const notify4 = () => toast.error('Account not Found!');
+  const notify5 = () => toast.warning('Arrive Soon!');
+  const [data,setData]=useState({
+    email:"",
+    password:"",
+  });
+  const handleData=(e)=>{
+    const{name,value}=e.target;
+    setData({
+      ...data,
+      [name]:value
+    })
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    if(data.email===""||data.password===""){
+      return notify1()
+    }
+    else{
+      try{
+        const response=await axios.post("http://localhost:1818/api/Aptifyr/LoginUser",data);
+        //console.log(response.data.message);
+        if(response.data.message==="LOGIN"){
+          return notify2()
+        }
+      }
+      catch(err){
+        //console.log(err.response.data.message);
+        if(err.response.data.message==="ACCOUNT"){
+          return notify4()
+        }
+        if(err.response.data.message==="PASSWORD"){
+          return notify3()
+        }
+      }
+    }
+  }
+  const not=()=>{
+    notify5()
+  }
   return (
     <>
+    <Header/>
+    <Mouse/>
+    <ToastContainer/>
     <div className="login">
     <div className="login-1">
     <div className="login-info">
     <h2>Login</h2>
     <p>Welcome back!Please log in to access your account.</p>
     </div>
-    <form>
-    <input type="text" placeholder="Enter Your Email" autoComplete="off"></input>
-    <input type="password" placeholder="Enter Your Password" autoComplete="off"></input>
-    <NavLink>Forgot Password?</NavLink>
-    <a><button onClick={notify}>LOGIN</button></a>
-    <NavLink to="/Signup"><button>SIGNUP</button></NavLink>
+    <form onSubmit={handleSubmit}>
+    <input type="text" placeholder="Enter Your Email" autoComplete="off" name="email" onChange={handleData}></input>
+    <input type="password" placeholder="Enter Your Password" autoComplete="off" name="password" onChange={handleData}></input>
+    <p onClick={not}>Forgot Password?</p>
+    <button type="submit">LOGIN</button>
+    <NavLink to='/Signup'><button>SIGNUP</button></NavLink>
     </form>
     </div>
     <div className="login-2">
+    <img src={PIC} alt="LOGIN IMAGE"></img>
     </div>
     </div>
     <ToastContainer/>
